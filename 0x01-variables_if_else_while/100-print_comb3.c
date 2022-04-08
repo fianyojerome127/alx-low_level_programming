7e32c5ee091b94b8ff1ade5360406d7d31aa6484
fianyojerome127 <fianyojerome127@gmail.com>
2022-04-08 15:16:16 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,25 +8,23 @@
  */
 int main(void)
 {
-	int d, p;
+	int digit1, digit2;
 
-	for (d = '0'; d < 9; d++)
+	for (digit1 = 0; digit1 < 9; digit1++)
 	{
-		for (p = d + 1; p < '9'; p++)
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 		{
-			if (p != d)
-			{
-				putchar(d);
-				putchar(p);
-				
-				if (d == '8' && p == '9')
-					continue;
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
 
-				putchar(',');
-				putchar(' ');
-			}
+			if (digit1 == 8 && digit2 == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
 		}
 	}
+
 	putchar('\n');
 
 	return (0);
