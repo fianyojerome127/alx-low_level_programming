ae94cb60695cca647803f96f662a9a6ae9804498
fianyojerome127 <fianyojerome127@gmail.com>
2022-04-12 18:36:50 -0700

    Task 14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - Prints first 50 fibonacci numbers, starting with 1 and 
- *        separated by a coma followed by a space.
+ * main - Prints first 50 Fibonacci numbers, starting with 1 and
+ *        sparated by a coma followed by a space.
  *
  * Return: Always 0.
  */
