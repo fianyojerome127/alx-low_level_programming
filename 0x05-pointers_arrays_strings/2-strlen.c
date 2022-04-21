67e91da74494ac53b98aac39b09c2826b9e72f84
fianyojerome127 <fianyojerome127@gmail.com>
2022-04-21 07:23:11 -0700

    Task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -7,9 +7,13 @@
  */
 int _strlen(char *s)
 {
-	int i = 0;
+	int i = 1, sum = 0;
+	char p1 = s[0];
 
-	while (s[i]) i++;
-
-	return (i);
+	while (p1 != '\0')
+	{
+		sum++;
+		p1 = s[i++];
+	}
+	return (sum);
 }
