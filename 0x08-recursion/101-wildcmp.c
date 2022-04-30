2368ad414ccde43f91c6f0f0ec02904908d25073
fianyojerome127 <fianyojerome127@gmail.com>
2022-04-30 12:40:22 +0000

    Task 9

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -3,13 +3,15 @@
 int strlen_no_wilds(char *str);
 void iterate_wild(char **wildstr);
 char *postfix_match(char *str, char *postfix);
+int wildcmp(char *s1, char *s2);
+
 
 int strlen_no_wilds(char *str)
 {
 	int len = 0, index = 0;
 	if (*(str + index))
 	{
-		if (*str != 'x')
+		if (*str != '*')
 			len++;
 		index++;
 
