c6ceae21d13716a9e61973b0922ea9965bb17d1b
fianyojerome127 <fianyojerome127@gmail.com>
2022-05-17 20:47:34 -0700

    task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,11 +3,11 @@
 
 /**
  * malloc_checked - allocate memory an checks.
-(* a blank line
- *@b: bits to allocate
-* Description: allocates memory)?
-(* section header: the header of this function is holberton.h)*
-* Return: this is void, no return.
+ * a blank line
+ * @b: bits to allocate
+ * Description: allocates memory?
+ * section header: the header of this function is main.h
+ * Return: this is void, no return.
 */
 void *malloc_checked(unsigned int b)
 {
