085394a9de7d3b9862c2d8b3e603df6adc896e0d
fianyojerome127 <fianyojerome127@gmail.com>
2022-06-04 12:18:26 -0700

    task 0

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,7 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef LINK_LIST_H
+#define LINK_LIST_H
+
+#include <stddef.h>
 
 /**
  * struct listint_s - singly linked list
@@ -7,19 +9,13 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
+ * for main project
  */
 typedef struct listint_s
 {
-int n;
-struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
-
-#include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
